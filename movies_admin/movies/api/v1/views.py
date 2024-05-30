19cdd673d8806af970bdccd4884ffe00fcee2c06
nafedoseev <nafedoseev@domclick.ru>
2024-05-30 22:22:46 +0300

    фикс после ревью

--- movies_admin/movies/api/v1/views.py
+++ movies_admin/movies/api/v1/views.py
@@ -7,11 +7,7 @@ from django.contrib.postgres.aggregates import ArrayAgg
 from movies.models import Filmwork, PersonFilmwork
 
 
-class MoviesListApi(BaseListView):
-    http_method_names = ['get']
-    paginate_by = 50
-    model = Filmwork
-
+class MoviesQuerysetMixin():
     def get_queryset(self):
         return (
             Filmwork.objects.all()
@@ -38,6 +34,12 @@ class MoviesListApi(BaseListView):
                 ),
             )
         )
+
+
+class MoviesListApi(MoviesQuerysetMixin, BaseListView):
+    http_method_names = ['get']
+    paginate_by = 50
+    model = Filmwork
 
     def get_context_data(self, *, object_list=None, **kwargs):
         queryset = self.get_queryset()
@@ -59,35 +61,8 @@ class MoviesListApi(BaseListView):
         return JsonResponse(context)
 
 
-class MoviesDetailApi(BaseDetailView):
+class MoviesDetailApi(MoviesQuerysetMixin, BaseDetailView):
     http_method_names = ['get']
-
-    def get_queryset(self):
-        return (
-            Filmwork.objects.all()
-            .prefetch_related('genres', 'persons')
-            .values(
-                'id', 'title', 'description', 'creation_date', 'rating', 'type'
-            )
-            .annotate(
-                genres=ArrayAgg('genres__name', distinct=True),
-                actors=ArrayAgg(
-                    'persons__full_name',
-                    distinct=True,
-                    filter=Q(personfilmwork__role=PersonFilmwork.Role.ACTOR)
-                ),
-                directors=ArrayAgg(
-                    'persons__full_name',
-                    distinct=True,
-                    filter=Q(personfilmwork__role=PersonFilmwork.Role.DIRECTOR)
-                ),
-                writers=ArrayAgg(
-                    'persons__full_name',
-                    distinct=True,
-                    filter=Q(personfilmwork__role=PersonFilmwork.Role.WRITER)
-                ),
-            )
-        )
 
     def render_to_response(self, context, **response_kwargs):
         return JsonResponse(context['object'])
